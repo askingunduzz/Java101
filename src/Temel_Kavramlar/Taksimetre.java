67a9dd3404a7441c7cd0e4a77dca20c2350762a5
Aşkın Gündüz <91720498+askingunduzz@users.noreply.github.com>
2022-08-14 12:42:30 +0300

    Add files via upload

--- src/Temel_Kavramlar/Taksimetre.java
+++ src/Temel_Kavramlar/Taksimetre.java
@@ -1,4 +1,22 @@
 package Temel_Kavramlar;
 
+import java.util.Scanner;
 public class Taksimetre {
+    public static void main(String[] args){
+        Scanner keyboard= new Scanner(System.in);
+
+        double km_basi=2.20;
+        int min=20;
+        int acilis=10;
+
+        System.out.print("Kac km gidildi? ");
+        double km=keyboard.nextDouble();
+
+        double ucret= acilis+km*km_basi;
+        double kesin_ucret= ucret<min ? min : ucret;
+        System.out.println("Ucretiniz: "+ kesin_ucret +" TL");
+
+
+
+    }
 }
