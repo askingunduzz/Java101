b9412f6f379e2abade831d33045b80f165b99f71
Aşkın Gündüz <91720498+askingunduzz@users.noreply.github.com>
2022-08-16 14:44:55 +0300

    Add files via upload

---
+++ src/Kosullu_Ifadeler/Hava_Sicakligi.java
@@ -0,0 +1,24 @@
+package Kosullu_Ifadeler;
+import java.util.Scanner;
+public class Hava_Sicakligi {
+    public static void main(String[] args){
+        Scanner keyboard= new Scanner(System.in);
+        int hava;
+        System.out.println("Hava sicakligini giriniz: ");
+        hava=keyboard.nextInt();
+
+        if(hava<5){
+            System.out.println("Kayak");
+        }
+        else if(hava>=5&&hava<15){
+            System.out.println("Sinema");
+        }
+        else if(hava>=15&&hava<25){
+            System.out.println("Piknik");
+        }
+        else{
+            System.out.println("Yuzme");
+        }
+
+    }
+}
