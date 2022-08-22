60881a8f75e9d25c68e99d57314db1be104a4cfa
Aşkın Gündüz <91720498+askingunduzz@users.noreply.github.com>
2022-08-22 11:35:50 +0300

    Add files via upload

---
+++ src/Kosullu_Ifadeler/Cin_Zodyagi.java
@@ -0,0 +1,67 @@
+package Kosullu_Ifadeler;
+
+import java.util.Scanner;
+
+public class Cin_Zodyagi {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Dogum yilinizi giriniz: ");
+        int yil=scan.nextInt();
+        int kalan=yil%12;
+        String hayvan;
+
+        switch(kalan){
+            case 0:
+                hayvan="Maymun";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 1:
+                hayvan="Horoz";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 2:
+                hayvan="Kopek";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 3:
+                hayvan="Domuz";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 4:
+                hayvan="Fare";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 5:
+                hayvan="Okuz";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 6:
+                hayvan= "Kaplan";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 7:
+                hayvan="Tavsan";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 8:
+                hayvan="Ejderha";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 9:
+                hayvan="Yilan";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 10:
+                hayvan="At";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+            case 11:
+                hayvan="Koyun";
+                System.out.println("Cin Zodyagi Burcunuz: "+ hayvan);
+                break;
+        }
+
+
+    }
+}
--- src/Kosullu_Ifadeler/Ucak_Bileti.java
+++ src/Kosullu_Ifadeler/Ucak_Bileti.java
@@ -1,46 +1,43 @@
 package Kosullu_Ifadeler;
+
 import java.util.Scanner;
+
 public class Ucak_Bileti {
-    public static void main(String[] args){
-        Scanner keyboard= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Mesafeyi km turunden giriniz : ");
-        int km= keyboard.nextInt();
+        int km = keyboard.nextInt();
 
         System.out.print("Yasinizi giriniz : ");
-        int yas=keyboard.nextInt();
+        int yas = keyboard.nextInt();
         double yas_indirim_orani;
-        if(yas<12){
-            yas_indirim_orani=0.5;
-        }
-        else if(yas>=12&&yas<=24){
-            yas_indirim_orani=0.1;
-        }
-        else if(yas>=65){
-            yas_indirim_orani=0.3;
-        }
-        else{
-            yas_indirim_orani=0;
+        if (yas < 12) {
+            yas_indirim_orani = 0.5;
+        } else if (yas >= 12 && yas <= 24) {
+            yas_indirim_orani = 0.1;
+        } else if (yas >= 65) {
+            yas_indirim_orani = 0.3;
+        } else {
+            yas_indirim_orani = 0;
         }
         double yolculuk_indirim;
         System.out.print("Yolculuk tipini giriniz (1=> Tek Yon, 2=> Gidis Donus ): ");
-        int yolcu_tipi=keyboard.nextInt();
-        if(yolcu_tipi==2){
-            yolculuk_indirim=0.2;
-        }
-        else{
-            yolculuk_indirim=0;
+        int yolcu_tipi = keyboard.nextInt();
+        if (yolcu_tipi == 2) {
+            yolculuk_indirim = 0.2;
+        } else {
+            yolculuk_indirim = 0;
         }
 
-        if(yolcu_tipi==1||yolcu_tipi==2) {
+        if (yolcu_tipi == 1 || yolcu_tipi == 2) {
             double normal_tutar = km * 0.10;
-            double yas_indirimi=normal_tutar*(yas_indirim_orani);
-            double indirimli_tutar=normal_tutar-yas_indirimi;
-            double gidis_donus_bilet_indirimi=indirimli_tutar*yolculuk_indirim;
-            double toplam_tutar=(indirimli_tutar-gidis_donus_bilet_indirimi)*yolcu_tipi;
-            System.out.print("Toplam Tutar = "+toplam_tutar+" TL ");
-        }
-        else{
+            double yas_indirimi = normal_tutar * (yas_indirim_orani);
+            double indirimli_tutar = normal_tutar - yas_indirimi;
+            double gidis_donus_bilet_indirimi = indirimli_tutar * yolculuk_indirim;
+            double toplam_tutar = (indirimli_tutar - gidis_donus_bilet_indirimi) * yolcu_tipi;
+            System.out.print("Toplam Tutar = " + toplam_tutar + " TL ");
+        } else {
             System.out.println("Hatali Veri Girdiniz!");
         }
     }
