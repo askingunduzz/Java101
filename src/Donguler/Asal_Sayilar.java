362ea7c36f61e826be7d8bba3df7603b50498d58
Aşkın Gündüz <91720498+askingunduzz@users.noreply.github.com>
2022-08-30 13:12:48 +0300

    Add files via upload

---
+++ src/Donguler/Asal_Sayilar.java
@@ -0,0 +1,21 @@
+package Donguler;
+
+public class Asal_Sayilar {
+    public static void main(String[] args) {
+        int sayac = 0;
+        System.out.println("1-100 arasindaki asal sayilar: ");
+
+        for (int i = 2; i <= 100; i++) {
+            for (int k = 2; k < i; k++) {
+                if (i % k == 0) {
+                    sayac++;
+                }
+            }
+            if (sayac == 0) {
+                System.out.print(i + " s");
+            } else {
+                sayac = 0;
+            }
+        }
+    }
+}
