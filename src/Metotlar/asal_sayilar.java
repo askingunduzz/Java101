3af45d0330617cf44781f2069dcbb585bfc21b2b
Aşkın Gündüz <91720498+askingunduzz@users.noreply.github.com>
2022-09-13 22:36:13 +0300

    Add files via upload

---
+++ src/Metotlar/asal_sayilar.java
@@ -0,0 +1,27 @@
+package Metotlar;
+import java.util.Scanner;
+public class asal_sayilar {
+    static String asal(int x,int trial){
+        if(x==2){
+            return " asal";
+        }
+        if(x%trial==0){
+           asal(x,trial+1);
+
+            return " asal degil.";
+        }
+        else{
+            return " asal.";
+        }
+
+
+
+    }
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Sayi giriniz: ");
+        int x=scan.nextInt();
+
+        System.out.println(x+asal(x,2));
+    }
+}
